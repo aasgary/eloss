c117fc36af7ee97de85ef04a776421a3ee4b80a7
aasgary <ali.asgary@gmail.com>
2024-05-05 14:02:11 -0400

    Home.py

--- Home.py
+++ Home.py
@@ -65,7 +65,7 @@ def show_location_on_map(lat, lon):
 
 # Streamlit interface setup
 st.title("Raster Value and Location Viewer")
-raster_path = st.text_input("Enter the path to the raster file:", 'https://drive.google.com/file/d/18T1EeRtO6WEaIfmkFgQ3o7P4XTARWL7a/view?usp=drive_link')
+raster_path = st.text_input("Enter the path to the raster file:", 'https://drive.google.com/file/d/1D2pASBmz3Q06d0vzY4o0cowRorTKqr-f/view?usp=sharing')
 latitude = st.number_input("Enter latitude:", value=56.23)
 longitude = st.number_input("Enter longitude:", value=-117.29)
 
