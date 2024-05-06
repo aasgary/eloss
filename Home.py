2851b7a111a54946719f0bac42e577eed116646f
aasgary <ali.asgary@gmail.com>
2024-05-06 09:49:21 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -50,4 +50,5 @@ if st.button('Get Flood Level Values and Show Location'):
             st.write(f"{flood_type}: {value}")
         map_display = show_map(latitude, longitude)
         map_html = map_display._repr_html_()
+        print(map_html)  # Check what is actually in map_html
         st.components.v1.html(map_html, height=500)
