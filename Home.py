dea905343e1f5ac98f39c02075a831db562623fb
aasgary <ali.asgary@gmail.com>
2024-05-05 20:51:17 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,15 @@ import rasterio
 from pyproj import Proj, Transformer
 import folium
 
-# Assuming flood_rasters dictionary is defined here
+# Flood_rasters dictionary 
+flood_rasters = {
+    "Flood 25": "https://drive.google.com/uc?export=download&id=1iLEuzYRML8Kb9qrPnXZppEuQS0D-68gy",
+    "Flood 50": "https://drive.google.com/uc?export=download&id=1z0ilA-WhpXa7-GhbC0jzGQiEiUS2yBXL",
+    "Flood 100": "https://drive.google.com/uc?export=download&id=1Z8pIbnNmz7fpH0Ecm2wqU8ayUo5Zf4E_",
+    "Flood 150": "https://drive.google.com/uc?export=download&id=18RcZJlZzvgc_cL1vKR1B6-9Ex3_xfV0-",
+    "Flood 200": "https://drive.google.com/uc?export=download&id=16yAjxqvui4fNgcdzRZyzto3Q2B94J_qU",
+   
+}
 
 # Function definitions
 def latlon_to_xy(lat, lon, dataset):
