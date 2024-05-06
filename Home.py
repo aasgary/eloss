33419fe1871c540f456c77b8492112b18a83e687
aasgary <ali.asgary@gmail.com>
2024-05-05 20:33:32 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -73,3 +73,22 @@ if st.button('Get Raster Value and Show Location'):
 
         map_html = map_display._repr_html_()
         st.components.v1.html(map_html, height=500)
+
+st.title("Interactive Map")
+
+col1, col2 = st.columns([4, 1])
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
+with col2:
+
+    basemap = st.selectbox("Select a basemap:", options, index)
+
+
+with col1:
+
+    m = leafmap.Map(
+        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+    )
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
